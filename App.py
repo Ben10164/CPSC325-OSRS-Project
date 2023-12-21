e941e661ad1206fceffdfbdcc0f38fdd126d748f
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2023-12-20 18:11:12 -0800

    Added caching

--- App.py
+++ App.py
@@ -13,12 +13,6 @@ st.set_page_config(
 )
 
 def convert_tf_to_pd(ds, limit=32):
-    """
-    Read data from Tensorflow dataset to Pandas dataframe
-    :param ds:
-    :param limit:
-    :return:
-    """
     batch_iterator = ds.batch(limit).make_one_shot_iterator()
     with tf.Session() as sess:
         batch = batch_iterator.get_next()
@@ -30,6 +24,9 @@ def convert_tf_to_pd(ds, limit=32):
 
     return pd.DataFrame.from_dict(samples)
 
+@st.cache_data
+def st_cache_get_data(name, view_delta):
+    return Predictor.get_data(name, view_delta)
 
 name = st.selectbox('Chose the Mega-Rare BIS item to view trading info for', ['Twisted bow' , 'Scythe of vitur (uncharged)', 'Tumeken\'s shadow (uncharged)'], label_visibility="visible")
 prediction_delta = st.selectbox('Delta used in Model and Data Display', ['1h' , '6h'])
@@ -42,7 +39,7 @@ else:
     view_delta = prediction_delta
 
 if len(name) != 0:
-    data = Predictor.get_data(name, view_delta)
+    data = st_cache_get_data(name, view_delta)
 
     cols1,_ = st.columns((1,2)) # To make it narrower
     format = 'MMM DD, YYYY'  # format output
@@ -55,33 +52,18 @@ if len(name) != 0:
 
     slider = cols1.slider('Select the start date of the graph', max_value=start_date.to_pydatetime(), value=end_date.to_pydatetime() ,min_value=end_date.to_pydatetime(), format=format)
 
-    # len = st.slider(label="# of datapoints (Large values cause performance issues)",max_value=data.index[-1], format='dddd, MMMM Do YYYY, h:mm:ss a', key=data.index)
-    # len = st.slider(label="# of datapoints (Large values cause performance issues)")
-    """
     # Current Data
-    """
     ChartHelper.get_altair_chart(data.loc[pd.to_datetime(slider):].reset_index(),name)
 
     MODEL = st.selectbox('Chose the Model to predict with', [None, 'Conv1D' , 'Linear', 'Multi_Step_Dense'], label_visibility="visible")
-    """
-    Note:  
-    Conv1D is the only model that predicts more than 1 step in the future. If you would like to use a Linear or Multi_Step_Dense model to predict, the value of Delta is how far in the future it will predict.
-    """
+    # Note:  
+    # Conv1D is the only model that predicts more than 1 step in the future.
+    # If you would like to use a Linear or Multi_Step_Dense model to predict, the value of Delta is how far in the future it will predict.
     if(MODEL is not None):
         model = Predictor.get_model(ITEM=name, DELTA=prediction_delta, MODEL=MODEL)
         test_df = DateTimeHelper.getDT(name, prediction_delta)[-30:]
         te = Predictor.predict(model, test_df)
         # st.write(te)
 
-
-        """
         # Predicted Data
-        """
         ChartHelper.get_altair_chart(data.loc[pd.to_datetime(slider):].reset_index(),name, te.numpy()[0][0])
-
-
-    # ChartHelper.get_sam_altair_chart(PredictorTest.getNeatGraph(name, time),name)
-    # get_predictor(name, prediction_delta)
-
-
-# # st.write(PredictorTest.data)
